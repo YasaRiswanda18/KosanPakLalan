550e55d62670d3f1eb20aced085a061b5fe5fc99
YasaRiswanda18 <arrazka183@gmail.com>
2026-09-12 00:01:46 +0700

    perbaiki tagihan notif

--- resources/views/admin/penghuni/index.blade.php
+++ resources/views/admin/penghuni/index.blade.php
@@ -93,6 +93,12 @@
                 </div>
 
                 <nav class="flex-1 px-4 py-6 space-y-1.5 overflow-y-auto">
+
+                <!-- RUMUS NOTIFIKASI ADMIN -->
+                    @php
+                        $notifTagihanAdmin = \App\Models\Tagihan::where('status', 'Menunggu Konfirmasi')->count();
+                        $notifKeluhanAdmin = \App\Models\Pengaduan::where('status', 'Pending')->count();
+                    @endphp
                     
                     <div class="px-3 pb-2 text-[11px] font-bold text-slate-400 uppercase tracking-wider">Menu Utama</div>
 
