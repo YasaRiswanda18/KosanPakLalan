575e1ead708d905ad85bf1b2cfaa02d4132752dd
YasaRiswanda18 <arrazka183@gmail.com>
2026-09-12 00:48:27 +0700

    perbaiki notif keluhan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -116,7 +116,7 @@
                     <!-- RUMUS NOTIFIKASI ADMIN -->
                     @php
                         $notifTagihanAdmin = \App\Models\Tagihan::where('status', 'Menunggu Konfirmasi')->count();
-                        $notifKeluhanAdmin = \App\Models\Pengaduan::where('status', 'Pending')->count();
+                        $notifKeluhanAdmin = \App\Models\Pengaduan::where('status', '!=', 'Selesai')->count();
                     @endphp
 
                     <div class="px-3 pb-2 text-[11px] font-bold text-slate-400 uppercase tracking-wider">
